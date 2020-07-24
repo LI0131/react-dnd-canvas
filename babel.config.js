9e89900b0ae374fe57c69d977d77e99223fd75d2
LI0131 <mccannliam.lm@gmail.com>
2020-07-23 21:29:12 -0400

    Update rollup config

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    presets: [
+        '@babel/react',
+    ],
+    plugins: [
+        '@babel/plugin-syntax-jsx',
+    ]
+};
--- config/rollup.config.js
+++ config/rollup.config.js
@@ -1,15 +1,45 @@
 import json from '@rollup/plugin-json';
+import babel from '@rollup/plugin-babel'
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import nodeGlobals from 'rollup-plugin-node-globals';
+import includePaths from 'rollup-plugin-includepaths';
+import { createFilter } from 'rollup-pluginutils';
+import { dependencies, peerDependencies } from '../package.json';
 
-const plugins = [
-    json()
-]
+const external = createFilter(
+    Object.keys({...dependencies, ...peerDependencies}).map(item => item),
+    null,
+    { resolve: false }
+);
+
+const extensions = [ '.js' ];
 
 export default {
     input: './src/index.js',
     output: {
         file: './dist/index.js',
-        format: 'umd',
+        format: 'esm',
         name: 'react-dnd-canvas'
     },
-    plugins
+    external,
+    plugins: [
+        babel({
+            exclude: 'node_modules/**',
+            extensions,
+            configFile: './babel.config'
+        }),
+        resolve({
+            browser: true
+        }),
+        nodeGlobals(),
+        json(),
+        commonjs(),
+        includePaths({
+            include: {},
+            paths: ['./src'],
+            external: [],
+            extensions: ['.js']
+        })
+    ]
 }
